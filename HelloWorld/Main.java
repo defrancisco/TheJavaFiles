7bce84d584991a34db7a699d2780dee84425aa63
delfina <delfina.franciscofrate@gmail.com>
2024-09-07 23:12:23 -0300

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -2,9 +2,7 @@
 public class Main{
 
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
-        int age;
-        age = 21;
-        System.out.println("I am "+ age + " years old.");
+        
+
     }
 }
