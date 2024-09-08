385dbde54bff3c85447f77d385aa8e4490573583
delfina <delfina.franciscofrate@gmail.com>
2024-09-07 23:00:01 -0300

    code

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -3,5 +3,8 @@ public class Main{
 
     public static void main(String[] args) {
         //System.out.println("Hello world!");
+        int age;
+        age = 21;
+        System.out.println("I am "+ age + " years old.");
     }
 }
