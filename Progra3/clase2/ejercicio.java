c647c02672296211f9c81659a3c40d7c6f46d210
delfina <delfina.franciscofrate@gmail.com>
2024-09-09 11:26:50 -0300

    clase 1

--- Progra3/clase2/ejercicio.java
+++ Progra3/clase2/ejercicio.java
@@ -1,5 +1,18 @@
 package Progra3.clase2;
 
 public class ejercicio {
+    public static void main(String[] args) {
+        int[][] mat =  {{4, 5, 6}, {7, 8, 9}, {5, 6, 7}};
+        int suma = 0;
+        for(int i = 0; i < mat.length; i++ ){ //1 + 2(n+1) +n = 3 + 2n
+            for(int j = 0; j < mat[i].length; j++){ //1 + 2(n+1) + n = 3 + 3n
+                suma += mat[i][j]; //2n
+            }
+
+        }
+        double promedio =  (double) suma / (mat.length * mat[0].length); // 2
+        System.out.println("Promedio: "+ promedio);
+        
+    }
     
 }
