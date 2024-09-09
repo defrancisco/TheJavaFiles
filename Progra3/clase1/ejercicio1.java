33968ffa10fb6cb564b0c6b5656644faed8bfdac
delfina <delfina.franciscofrate@gmail.com>
2024-09-09 10:56:05 -0300

    my notes

---
+++ Progra3/clase1/ejercicio1.java
@@ -0,0 +1,12 @@
+package Progra3.clase1;
+/*
+ * Dada esta matriz: int[][] mat = {{4,5,6},{7,8,9},{5,6,7}};
+Realizar un programa en java para calcular el promedio.
+Para los cálculos, asumir que la matriz es cuadrada
+Realizar el conteo de instrucciones.
+Calcular la complejidad asintótica
+ */
+
+public class ejercicio1 {
+    
+}
