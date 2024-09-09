229b02ed980f8a0f7f025b3660752a392674de3a
defrancisco <delfina.franciscofrate@gmail.com>
2024-09-09 14:54:04 -0300

    en clase

---
+++ Progra3/clase6/SeleccionDeJugador.java
@@ -0,0 +1,9 @@
+package Progra3.clase6;
+
+public class SeleccionDeJugador{
+    public static void main(String[] args) {
+        Jugador[] jugadores = {
+            new Jugador()
+        }
+    }
+}
